1435458149d962cf9ac0fdabc04cad7616ae7f5f
BenoitLafabrie <[benoit_lafabrie_pro@fastmail.com](mailto:benoit_lafabrie_pro@fastmail.com)>
2023-04-12 12:06:15 +0200

    branch initiallizationn + font & logos added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import BurgerMenu from "./components/BurgerMenu";
 
 import Home from "./pages/Home";
 
