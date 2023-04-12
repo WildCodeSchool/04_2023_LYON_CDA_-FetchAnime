44530c514489d695f64a8cf9accb1bfde39eb444
IdirX69 <adil.idirr@gmail.com>
2023-04-12 10:45:39 +0200

    first commit with router and mui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
-import Home from "./pages/Home";
+/* eslint-disable import/no-extraneous-dependencies */
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <p>coucou</p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
